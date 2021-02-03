a1d5d27f6879ac65c14ab6b8c0faad1dff13da7c
Emilio Araos <ea3dev@gmail.com>
2021-02-02 22:37:20 -0800

    login and db

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,28 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_required, login_user, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('get')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully', 'success')
+                login_user(user)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect Password. Try again', 'error')
+        else:
+            flash('Email does not exist', 'error')
+
     data = request.form
     print(data)
     return render_template('login.html')
@@ -19,19 +37,26 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', 'error')
+        elif len(email) < 4:
             flash('Email must be grater than 3 chars', 'error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name should be at least one char long', 'error')
-        elif len(password1) < 2:
-            flash('Password should be at least one char long', 'error')
-        elif len(password2) < 2:
-            flash('Password should be at least one char long', 'error')
-        elif (len(password1) == len(password2)) and (len(password1) and len(password2) > 2):
+        elif password1 != password2:
+            flash('Passwords do not match', 'error')
+        elif len(password1) > 7:
+            flash('Password must be greater than 6 characters', 'error')
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created', 'success')
+            return redirect(url_for('views.home'))
 
     return render_template('sign_up.html')
--- website/models.py
+++ website/models.py
@@ -1,5 +1,6 @@
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 
 class User(db.Model, UserMixin):
@@ -7,5 +8,13 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     first_name = db.Column(db.String(100))
+    notes = db.relationship('Note')
+    # Child  relation is capitalized: Note
 
-    
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(2000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    # One to many relationship. Note is the child. User the parent.
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
